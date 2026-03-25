d00c6d78d6d769f810d1afa49708a0c858280d20
HMZJN\RiverSoth <zjnlove@foxmai.com>
2026-03-25 21:41:28 +0800

    提交修改

--- src/themes/aiship/blocks/hero.tsx
+++ src/themes/aiship/blocks/hero.tsx
@@ -284,11 +284,11 @@ export function Hero({
       <ParticleBackground />
 
       {/* 渐变背景光晕 */}
-      <div className="pointer-events-none absolute inset-0 z-0">
+      {/* <div className="pointer-events-none absolute inset-0 z-0">
         <div className="absolute top-1/4 left-1/4 h-96 w-96 rounded-full bg-green-500/20 blur-3xl" />
         <div className="absolute top-1/2 right-1/4 h-96 w-96 rounded-full bg-emerald-500/20 blur-3xl" />
         <div className="absolute bottom-1/4 left-1/2 h-96 w-96 rounded-full bg-teal-500/10 blur-3xl" />
-      </div>
+      </div> */}
       {section.announcement && (
         <FadeInDown delay={0}>
           <Link
