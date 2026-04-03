89bdb76b9320b0ec0669d98b8408e048cdb11704
HMZJN\RiverSoth <zjnlove@foxmai.com>
2026-04-03 19:11:30 +0800

    可伶2.6修改完

--- src/shared/blocks/generator/video-config/models/kling.ts
+++ src/shared/blocks/generator/video-config/models/kling.ts
@@ -199,59 +199,46 @@ export const klingModels: VideoModelOption[] = [
     brand: 'kling',
     modelPath: 'kling-2-6',
     baseCredits: {
-      'text-to-video': 4,
-      'image-to-video': 6,
+      'text-to-video': 55,
+      'image-to-video': 55,
     },
+    maxImages: 1,
     customOptions: {
       aspectRatio: [
         { value: '16:9', label: 'advanced_options.aspect_ratio_options.16_9' },
         { value: '9:16', label: 'advanced_options.aspect_ratio_options.9_16' },
         { value: '1:1', label: 'advanced_options.aspect_ratio_options.1_1' },
       ],
-      resolution: [
-        { value: '720', label: 'advanced_options.resolution_options.720p' },
-        { value: '1080', label: 'advanced_options.resolution_options.1080p' },
-      ],
       duration: [
         { value: '5', label: 'advanced_options.duration_options.5s' },
         { value: '10', label: 'advanced_options.duration_options.10s' },
-        { value: '15', label: 'advanced_options.duration_options.15s' },
       ],
     },
     creditRules: [
       {
-        conditions: { resolution: '720' },
-        credits: 8,
-        perUnit: true,
-        unitField: 'duration',
+        conditions: { duration: '10' },
+        credits: 55,
       },
       {
-        conditions: { resolution: '1080' },
-        credits: 15,
-        perUnit: true,
-        unitField: 'duration',
+        conditions: { duration: '5', audio: true },
+        credits: 55,
       },
       {
-        conditions: { resolution: '720', audio: true },
-        credits: 4,
-      },
-      {
-        conditions: { resolution: '1080', audio: true },
-        credits: 8,
+        conditions: { duration: '10', audio: true },
+        credits: 110,
       },
     ],
     sceneValues: {
-      'text-to-video': 'kling-2-6-text-to-video',
-      'image-to-video': 'kling-2-6-image-to-video',
+      'text-to-video': 'kling-2.6/text-to-video ',
+      'image-to-video': 'kling-2.6/image-to-video',
     },
     defaultOptions: {
       aspect_ratio: '16:9',
-      resolution: '720',
       duration: '5',
       audio: false,
     },
     advancedOptions: {
-      supportedTypes: ['aspectRatio', 'resolution', 'duration', 'audio'],
+      supportedTypes: ['aspectRatio', 'duration', 'audio'],
     },
   },
   {
